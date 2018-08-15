0fe1f59a865104b9af0e2a43813f684bb91ea957
emd5 <emahoney5@mail.greenriver.edu>
2018-08-15 00:00:38 -0700

    add functions to create receipt

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,41 +1,41 @@
 from textwrap import dedent
 
 WIDTH = 50
-MENU = [{'Appetizers': [{'items':'Wings', 'price' : 20.99},
-                        {'items':'Cookies', 'price' : 1.50},
-                        {'items':'Spring Rolls', 'price' : 2.00},
-                        {'items':'Armadillo eggs', 'price' : 4.35},
-                        {'items':'Jalapeno Poppers', 'price' : 10.50},
-                        {'items':'Fries', 'price' : 2.00}]},
-         {'Entrees': [{'items':'Salmon', 'price' : 20.99},
-                        {'items':'Steak', 'price' : 1.50},
-                        {'items':'Meat Tornado', 'price' : 2.00},
-                        {'items':'A Literal Garden', 'price' : 4.35},
-                        {'items':'Surf and Turf', 'price' : 10.50},
-                        {'items':'Ribeye', 'price' : 2.00}
-                      ]},
-        {'Desserts': [{'items':'Ice cream', 'price' : 1.99},
-                        {'items':'Cake', 'price' : 2.50},
-                        {'items':'Pie', 'price' : 2.99},
-                        {'items':'Cupcake', 'price': 3.35},
-                        {'items':'Macaroon', 'price': 5.50},
-                        {'items':'Coconut', 'price' : 1.00}
-                      ]},
-        {'Drinks': [{'items':'Coffee', 'price' : 20.99},
-                        {'items':'Tea', 'price' : 1.50},
-                        {'items':'Blood of the Innocent', 'price' : 2.00},
-                        {'items':'Whiskey', 'price' : 4.35},
-                        {'items':'Vodka', 'price' : 10.50},
-                        {'items':'Sparkling Wine', 'price' : 2.00}
-                      ]},
-        {'Sides': [{'items':'Veggies', 'price' : 5.99},
-                        {'items':'Fries', 'price' : 2.50},
-                        {'items':'Potato', 'price' : 2.00},
-                        {'items':'Garlic', 'price' : 9.35},
-                        {'items':'Biscuits', 'price' : 6.50},
-                        {'items':'Mashed Tots', 'price' : 7.00}
-                      ]},
-        ]
+MENU = {'Appetizers': [{ 'Wings': 20.99},
+                        {'Cookies': 1.50},
+                        {'Spring Rolls': 2.00},
+                        {'Armadillo eggs': 4.35},
+                        {'Jalapeno Poppers': 10.50},
+                        {'Fries': 2.00}],
+        'Entrees': [{'Salmon': 20.99},
+                     {'Steak': 1.50},
+                     {'Meat Tornado': 2.00},
+                     {'A Literal Garden': 4.35},
+                     {'Surf and Turf': 10.50},
+                     {'Ribeye': 2.00}
+                     ],
+        'Desserts': [{'Ice cream': 1.99},
+                      {'Cake': 2.50},
+                      {'Pie': 2.99},
+                      {'Cupcake': 3.35},
+                      {'Macaroon': 5.50},
+                      {'Coconut': 1.00}
+                      ],
+        'Drinks': [{'Coffee': 20.99},
+                    {'Tea': 1.50},
+                    {'Blood of the Innocent': 2.00},
+                    {'Whiskey': 4.35},
+                    {'Vodka': 10.50},
+                    {'Sparkling Wine': 2.00}
+                    ],
+        'Sides': [{'Veggies': 5.99},
+                   {'Fries': 2.50},
+                   {'Potato': 2.00},
+                   {'Garlic': 9.35},
+                   {'Biscuits': 6.50},
+                   {'Mashed Tots': 7.00}
+                   ]
+        }
 
 
 def greeting():
@@ -54,14 +54,18 @@ def greeting():
         {(' ' * ((WIDTH - len(ln_three)) // 2)) + ln_three + (' ' * ((WIDTH - len(ln_three)) // 2))}
         {'*' * WIDTH}
     '''))
-    for item in MENU:
-        for k, v in item.items():
-            print(k)
-            print('-' * 10)
-            for food_items in v:
-                print(food_items.get('items'))
-            print()
+    print_menu()
 
+
+def print_menu():
+    for k,v in MENU.items():
+        print(k)
+        print('-' * 10)
+        for item in v:
+            for key in item.keys():
+                print(key)
+                # print(food_items.get('price'))
+        print()
     print('*' * 35)
     print('** What would you like to order? **')
     print('*' * 35)
@@ -77,13 +81,21 @@ def check_input(user_input):
     if user_input.lower() == "quit":
         exit()
         return
-    for food in MENU:
-        for k, v in food.items():
-            for food_items in v:
-                if user_input.lower() == food_items.get('items').lower():
+    for k,v in MENU.items():
+        for item in v:
+            for key in item.keys():
+                if user_input.lower() == key.lower():
                     return True
     else:
         return False
+
+
+def find_item_price(user_input):
+    for key, value in MENU.items():
+        for price in value:
+            for k, v in price.items():
+                if user_input  == k.lower():
+                    return(v)
 
 
 def create_order(user_input, order_items):
@@ -94,26 +106,75 @@ def create_order(user_input, order_items):
     return order_items
 
 
+def find_item_price(user_input):
+    for key, value in MENU.items():
+        for price in value:
+            for k, v in price.items():
+                if user_input  == k.lower():
+                    return(v)
+
+
+def calculate_item_price(order_dict):
+    receipt = []
+    for k, v in order_dict.items():
+        price = find_item_price(k)
+        count = order_dict[k]
+        total = price * count
+        receipt_dict = {'item': k, 'count': v, 'total': total}
+        receipt.append(receipt_dict)
+    return receipt
+
+
+def calculate_total_price(receipt_dict):
+    sum_total = 0
+    for price in receipt_dict:
+       sum_total += price['total']
+    return sum_total
+
+
+def print_receipt(order_list):
+    total = calculate_item_price(order_list)
+    total_price = calculate_total_price(total)
+    print('*' * 50)
+    print('The Snakes Cafe')
+    print('Eatability Counts')
+    print()
+    print('Order')  # UUID
+    print('=' * 50)
+    for items in total:
+        print('{} x{} {:>40}'.format(items['item'], items['count'], items['total']))
+        #items, count, price
+    print('-' * 50)
+    #subtotal
+    #sales tax
+    print('-' * 10)
+    print('Total {:>40}'.format(total_price))
+    print('*' * 50)
+
+
 def run():
     greeting()
     order_list = {}
 
     while True:
         menu_order = ask_question()
-
         if menu_order == 'order':
-            #create print_order function
-            pass
+            print_receipt(order_list)
         elif menu_order == 'menu':
-            greeting()
+            print_menu()
         elif 'remove' in menu_order:
             #remove single item from list
             pass
         elif check_input(menu_order) is True:
             order_list = create_order(menu_order,order_list)
-            print('{} order of {} have been added to the meal'.format(order_list[menu_order],menu_order))
+            print('{} order of {} have been added to the meal'.format(order_list[menu_order], menu_order))
+            print()
         else:
             ask_question()
+
+        # for food in order_list:
+        #     if food['items'] == menu_order:
+        #         print('{} order of {} have been added to your meal'.format(food['count'], food['items']))
 
 
 if __name__ == '__main__':
