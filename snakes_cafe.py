6dc4667075f9c272dab52219b2733611eb99d1f3
emd5 <emahoney5@mail.greenriver.edu>
2018-08-14 21:55:17 -0700

    fixed increment food counter, add functions for today's assignment

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,11 +1,41 @@
 from textwrap import dedent
 
 WIDTH = 50
-MENU = {'Appetizers': ['Wings', 'Cookies', 'Spring Rolls'],
-        'Entrees': ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden'],
-        'Desserts': ['Ice Cream', 'Cake', 'Pie'],
-        'Drinks': ['Coffee', 'Tea', 'Blood of the Innocent'],
-        }
+MENU = [{'Appetizers': [{'items':'Wings', 'price' : 20.99},
+                        {'items':'Cookies', 'price' : 1.50},
+                        {'items':'Spring Rolls', 'price' : 2.00},
+                        {'items':'Armadillo eggs', 'price' : 4.35},
+                        {'items':'Jalapeno Poppers', 'price' : 10.50},
+                        {'items':'Fries', 'price' : 2.00}]},
+         {'Entrees': [{'items':'Salmon', 'price' : 20.99},
+                        {'items':'Steak', 'price' : 1.50},
+                        {'items':'Meat Tornado', 'price' : 2.00},
+                        {'items':'A Literal Garden', 'price' : 4.35},
+                        {'items':'Surf and Turf', 'price' : 10.50},
+                        {'items':'Ribeye', 'price' : 2.00}
+                      ]},
+        {'Desserts': [{'items':'Ice cream', 'price' : 1.99},
+                        {'items':'Cake', 'price' : 2.50},
+                        {'items':'Pie', 'price' : 2.99},
+                        {'items':'Cupcake', 'price': 3.35},
+                        {'items':'Macaroon', 'price': 5.50},
+                        {'items':'Coconut', 'price' : 1.00}
+                      ]},
+        {'Drinks': [{'items':'Coffee', 'price' : 20.99},
+                        {'items':'Tea', 'price' : 1.50},
+                        {'items':'Blood of the Innocent', 'price' : 2.00},
+                        {'items':'Whiskey', 'price' : 4.35},
+                        {'items':'Vodka', 'price' : 10.50},
+                        {'items':'Sparkling Wine', 'price' : 2.00}
+                      ]},
+        {'Sides': [{'items':'Veggies', 'price' : 5.99},
+                        {'items':'Fries', 'price' : 2.50},
+                        {'items':'Potato', 'price' : 2.00},
+                        {'items':'Garlic', 'price' : 9.35},
+                        {'items':'Biscuits', 'price' : 6.50},
+                        {'items':'Mashed Tots', 'price' : 7.00}
+                      ]},
+        ]
 
 
 def greeting():
@@ -24,12 +54,12 @@ def greeting():
         {(' ' * ((WIDTH - len(ln_three)) // 2)) + ln_three + (' ' * ((WIDTH - len(ln_three)) // 2))}
         {'*' * WIDTH}
     '''))
-
-    for k, v in MENU.items():
-        print(k)
-        print('-' * 10)
-        for item in v:
-            print(item)
+    for item in MENU:
+        for k, v in item.items():
+            print(k)
+            print('-' * 10)
+            for food_items in v:
+                print(food_items.get('items'))
             print()
 
     print('*' * 35)
@@ -41,31 +71,47 @@ def ask_question():
     return input()
 
 
-def check_input(user_input, menu):
+def check_input(user_input):
+    """validates user input, otherwise exits program
+    """
     if user_input.lower() == "quit":
         exit()
         return
-    for k, v in menu.items():
-        for item in v:
-            if user_input.lower() == item.lower():
-                return True
+    for food in MENU:
+        for k, v in food.items():
+            for food_items in v:
+                if user_input.lower() == food_items.get('items').lower():
+                    return True
     else:
         return False
 
 
+def create_order(user_input, order_items):
+    if user_input not in order_items.keys():
+        order_items[user_input] = 1
+    else:
+        order_items[user_input] += 1
+    return order_items
+
+
 def run():
     greeting()
-    order_counter = 0
-    while True:
-        order_list = []
-        menu_order = ask_question()
-        if check_input(menu_order, MENU) is True:
-            order_list.append(menu_order)
-            if menu_order in order_list:
-                order_counter += 1
-                print('{} order of {} have been added to your meal'.format(order_counter, menu_order))
-                print()
+    order_list = {}
 
+    while True:
+        menu_order = ask_question()
+
+        if menu_order == 'order':
+            #create print_order function
+            pass
+        elif menu_order == 'menu':
+            greeting()
+        elif 'remove' in menu_order:
+            #remove single item from list
+            pass
+        elif check_input(menu_order) is True:
+            order_list = create_order(menu_order,order_list)
+            print('{} order of {} have been added to the meal'.format(order_list[menu_order],menu_order))
         else:
             ask_question()
 
